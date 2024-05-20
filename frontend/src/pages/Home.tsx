5935d9b3ee26a886603ec21e45fbac9e43e86e1b
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-05-20 00:26:32 -0400

    test components page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,22 +1,15 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './css/Home.css';
+import NavBarLogin from '../components/NavBarLogin';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
+          <NavBarLogin></NavBarLogin>
         </IonHeader>
-        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
