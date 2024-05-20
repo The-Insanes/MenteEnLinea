8d17abb542410c80649b2fc20dbcbda1be725267
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-05-20 12:48:21 -0400

    update page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,16 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './css/Home.css';
 import NavBarLogin from '../components/NavBarLogin';
+import Login from '../components/Login';
+import FooterPage from '../components/FooterPage';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+      <IonHeader>
+        <IonToolbar>
           <NavBarLogin></NavBarLogin>
-        </IonHeader>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        
       </IonContent>
+      <IonFooter>
+        <FooterPage background='primary'></FooterPage>
+      </IonFooter>
     </IonPage>
   );
 };
