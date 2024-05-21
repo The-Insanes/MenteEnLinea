73dbc120ad3cb15a07d06fe4f7ae9bb69f4ab265
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-05-21 19:51:40 -0400

    fix bugs with these components

--- frontend/src/components/FooterPage.tsx
+++ frontend/src/components/FooterPage.tsx
@@ -5,23 +5,26 @@ import { mail, call } from 'ionicons/icons';
 interface ContainerProps {
     className?: string | undefined
     background?: string | undefined,
+    email?: string | "mail@example.cl",
+    phone_number?: string | "+5699999999",
+    src_logo?: string | undefined
 };
 
-const FooterPage: React.FC<ContainerProps> = ({className, background}) => {
+const FooterPage: React.FC<ContainerProps> = ({className, background, email, phone_number, src_logo}) => {
     return (
-        <IonToolbar color={background}>
+        <IonToolbar className={className} color={background}>
             <div className='info_footer_menu'>
                 <IonLabel className='title_footer_menu'>Contacto</IonLabel>
                 <IonLabel className='info_footer'>
                     <IonIcon slot='start' icon={call}></IonIcon>
-                    +5699999999
+                    {email}
                 </IonLabel>
                 <IonLabel className='info_footer'>
                     <IonIcon slot='start' icon={mail}></IonIcon>
-                    mail@example.cl
+                    {phone_number}
                 </IonLabel>
             </div>
-            <IonImg className='footer_dark_logo' src='images/dark_logo.svg' slot='end'></IonImg>
+            <IonImg className='footer_dark_logo' src={src_logo} slot='end'></IonImg>
         </IonToolbar>
     );
 };
