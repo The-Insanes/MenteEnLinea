3d1da01eef7b87ac02fe4f39e0074f10b271f3d2
Joaquin Tapia <joaquin.tapia.m01@mail.pucv.cl>
2024-05-26 11:34:37 -0400

    no use for navbarSignup

---
+++ frontend/src/components/NavbarSignup.tsx
@@ -0,0 +1,12 @@
+import './css/NavbarSignup.css';
+import { IonMenu, IonSplitPane } from '@ionic/react';
+
+const NavbarSignup: React.FC = () =>{
+    return (
+        <IonSplitPane>
+            <IonMenu>MENU</IonMenu>
+        </IonSplitPane>
+)
+}
+
+export default NavbarSignup
