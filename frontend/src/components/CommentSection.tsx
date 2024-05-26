a9315e70ccc335d75c5eb6de37ac3388c62a152d
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-05-26 00:21:30 -0400

    finished commentSection component

--- frontend/src/components/CommentSection.tsx
+++ frontend/src/components/CommentSection.tsx
@@ -1,3 +1,4 @@
+import './css/CommentSection.css'
 import React, { useState, useEffect } from 'react';
 import {
   IonContent,
@@ -21,45 +22,57 @@ interface ContainerProps {
   comments?: Comment[];
 }
 
+const PAGE_SIZE = 10; // Número de elementos por página
+
 const CommentSection: React.FC<ContainerProps> = ({ className, comments }) => {
   const [items, setItems] = useState<Comment[]>([]);
+  const [currentPage, setCurrentPage] = useState(0);
 
-  const generateItems = () => {
-    const newItems: Comment[] = [];
+  const loadMoreItems = () => {
+    if (!comments) return;
 
-    if (comments) {
-      for (let i = 0; i < comments.length; i++) {
-        newItems.push(comments[i]);
-      }
-    }
+    const nextPage = currentPage + 1;
+    const startIndex = currentPage * PAGE_SIZE;
+    const endIndex = nextPage * PAGE_SIZE;
+    const newItems = comments.slice(startIndex, endIndex);
+
     setItems(prevItems => [...prevItems, ...newItems]);
+    setCurrentPage(nextPage);
   };
 
   useEffect(() => {
-    generateItems();
+    setItems([]); // Resetear elementos al cambiar los comentarios
+    setCurrentPage(0); // Resetear la página al cambiar los comentarios
+    loadMoreItems(); // Cargar la primera página
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [comments]);
 
   return (
-    <IonContent className={className}>
-      <IonList>
-        {items.map((item, index) => (
-            <CommentLabel labelName={item.labelName}
-              labelInfo={item.labelInfo}
-              labelColor={index % 2 === 0 ? '#4F80FF' : '#F7AEF8'}
-              elipseColor={index % 2 === 0 ? '#FF4DE6' : '#4F80FF'}
-            />
-        ))}
-      </IonList>
-      <IonInfiniteScroll
-        onIonInfinite={(ev) => {
-          generateItems();
-          setTimeout(() => ev.target.complete(), 500);
-        }}
-      >
-        <IonInfiniteScrollContent></IonInfiniteScrollContent>
-      </IonInfiniteScroll>
-    </IonContent>
+    <div className={className}>
+      <div className='container-comment-section'>
+          <IonContent>
+              <IonList>
+                  {items.map((item, index) => (
+                  <CommentLabel
+                      labelName={item.labelName}
+                      labelInfo={item.labelInfo}
+                      labelColor={index % 2 === 0 ? '#4F80FF' : '#F7AEF8'}
+                      elipseColor={index % 2 === 0 ? '#FF4DE6' : '#4F80FF'}
+                  />
+                  ))}
+              </IonList>
+              <IonInfiniteScroll
+                  onIonInfinite={(ev) => {
+                  loadMoreItems();
+                  setTimeout(() => ev.target.complete(), 500);
+                  }}
+                  
+              >
+                  <IonInfiniteScrollContent loadingText="Porfavor Espere..." loadingSpinner="bubbles"></IonInfiniteScrollContent>
+              </IonInfiniteScroll>
+          </IonContent>
+        </div>
+    </div>
   );
 };
 
