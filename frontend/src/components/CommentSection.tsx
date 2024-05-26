d1d596d4d8878450bc56a43b8d925da80426e9e5
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-05-25 21:00:02 -0400

    add infinite scroll

--- frontend/src/components/CommentSection.tsx
+++ frontend/src/components/CommentSection.tsx
@@ -1,18 +1,66 @@
-import { IonInfiniteScroll } from '@ionic/react';
-import './css/CommentSection.css'
+import React, { useState, useEffect } from 'react';
+import {
+  IonContent,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  IonList,
+  IonItem,
+  IonAvatar,
+  IonLabel,
+  IonToolbar,
+} from '@ionic/react';
+import CommentLabel from './CommentLabel';
+
+interface Comment {
+  labelName: string;
+  labelInfo: string;
+}
 
 interface ContainerProps {
-    className?: string | undefined
-};
+  className?: string;
+  comments?: Comment[];
+}
 
-const CommentSection: React.FC<ContainerProps> = ({className}) => {
-    return (
-        <div className={className}>
-            <div className='container-comment-section'>
-                <IonInfiniteScroll></IonInfiniteScroll>
-            </div>
-        </div>
-    );
+const CommentSection: React.FC<ContainerProps> = ({ className, comments }) => {
+  const [items, setItems] = useState<Comment[]>([]);
+
+  const generateItems = () => {
+    const newItems: Comment[] = [];
+
+    if (comments) {
+      for (let i = 0; i < comments.length; i++) {
+        newItems.push(comments[i]);
+      }
+    }
+    setItems(prevItems => [...prevItems, ...newItems]);
+  };
+
+  useEffect(() => {
+    generateItems();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [comments]);
+
+  return (
+    <IonContent className={className}>
+      <IonList>
+        {items.map((item, index) => (
+            <CommentLabel labelName={item.labelName}
+              labelInfo={item.labelInfo}
+              labelColor={index % 2 === 0 ? '#4F80FF' : '#F7AEF8'}
+              elipseColor={index % 2 === 0 ? '#FF4DE6' : '#4F80FF'}
+            />
+        ))}
+      </IonList>
+      <IonInfiniteScroll
+        onIonInfinite={(ev) => {
+          generateItems();
+          setTimeout(() => ev.target.complete(), 500);
+        }}
+      >
+        <IonInfiniteScrollContent></IonInfiniteScrollContent>
+      </IonInfiniteScroll>
+    </IonContent>
+  );
 };
 
 export default CommentSection;
--- frontend/src/pages/PsychologistPresentation.tsx
+++ frontend/src/pages/PsychologistPresentation.tsx
@@ -4,8 +4,24 @@ import FooterPage from "../components/FooterPage";
 import NavBarLogin from "../components/NavBarLogin";
 import ProfileLabel from "../components/ProfileLabel";
 import CommentLabel from '../components/CommentLabel';
+import CommentSection from '../components/CommentSection';
+
+const generateItems = () => {
+    const comments = []
+
+    for(let i = 0; i < 50; i++) {
+        comments.push({
+            labelName: `User ${i + 1}`,
+            labelInfo: 'Breve opinión del Psicologo'
+        });
+    }
+
+    return comments
+}
 
 const PychologistPage: React.FC = () => {
+    const comments = generateItems()
+    
     return (
         <IonPage>
             <IonHeader>
@@ -17,8 +33,9 @@ const PychologistPage: React.FC = () => {
                     <ProfileLabel price="XXXXX" method="Online" psychologist_name="Barbara"></ProfileLabel>
                 </div>
 
-                <CommentLabel></CommentLabel>
-                <IonLabel> Opiniones: </IonLabel>
+                <CommentLabel labelName='Descripción' labelInfo='Breve descripción del psicologo'></CommentLabel>
+                <IonLabel>Opiniones: </IonLabel>
+                <CommentSection comments={comments}></CommentSection>
             </IonContent>
 
             <IonFooter>
