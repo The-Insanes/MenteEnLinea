fdbf1ffd6cef82da60b8f43c1a968e59d36e682d
MOWENFML <fabrizzioaml@gmail.com>
2024-05-25 20:20:50 -0400

    finished component, added new divs for doctorimagecontainer in leftside, added new divs for punctuation, name, area and prevision in rightside

--- frontend/src/components/PsychologistSummary.tsx
+++ frontend/src/components/PsychologistSummary.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './css/PsychologistSummary.css';
+import { IonAvatar } from '@ionic/react';
+
 
 interface ContainerProps { 
     className?: string | undefined
@@ -8,9 +10,24 @@ interface ContainerProps {
 const PsychologistSummary: React.FC<ContainerProps> = ({className}) => { 
     return (
         <div id = 'pill'>
+            <div id = 'leftSide'>
+                <div id = 'DoctorImageContainer'>
+                    <img id = 'perfil' alt="isaac" src="isaacProfile.jpg" />
+                </div>
+            </div>
+            <div id = 'rightSide'>
+                <div id = 'punctuation'><img src="punctuation_dr_house.svg" alt="Punctuation" /></div>
+                <div id = 'name'>
+                    <p>Nombre: Dr. Isaac</p>
+                </div>
+                <div id = 'AreaResume'>
+                    <p>Area de atención: Familia</p>
+                </div>
+                <div id = 'Previsión'>
+                    <p>Previsión: Fonasa</p>
+                </div>
 
-            <div id = 'leftSide'></div>
-            <div id = 'rightSide'></div>
+            </div>
 
         </div>
     );
