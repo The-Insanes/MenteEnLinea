8306d4fdd1f6b84e39ec92d0a6ad5e83e8ad897b
Joaquin Tapia <joaquin.tapia.m01@mail.pucv.cl>
2024-05-26 11:32:37 -0400

    Menu is now not showed in navbar, but in the whole page

--- frontend/src/components/NavBarLogin.tsx
+++ frontend/src/components/NavBarLogin.tsx
@@ -5,29 +5,16 @@ interface ContainerProps {
     className?: string | undefined
     
 };
-const paths = [
-    {name: 'Log in', url: 'LoginPage'},
-    {name: 'Sign up', url: 'SignupPage'},
-]
 
 const NavBarLogin: React.FC<ContainerProps> = ({className}) => {
     return (
+
         
         <IonToolbar className={className}>
             <IonButton className='logo_button' fill='clear' slot='start'>
                 <IonImg src='logo.svg'></IonImg>
             </IonButton>
-            <IonMenu contentId="main-content">
-                        <IonContent className="ion-padding">
-                            {paths.map((item, index)=> ( 
-                                <IonMenuToggle>
-                                    <IonItem routerLink={item.url} routerDirection='none'>
-                                    {item.name}
-                                    </IonItem>
-                                </IonMenuToggle>
-                            ))}
-                        </IonContent>
-                    </IonMenu>       
+                 
                 <IonButtons  id='main-content'slot="end">
                     <IonMenuButton  className='option_button' autoHide={false} color='primary'></IonMenuButton>
                 </IonButtons>
