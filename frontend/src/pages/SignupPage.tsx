0a6e579df89826d778d4e3aa36c9cb1ddaa9e3e3
Joaquin Tapia <joaquin.tapia.m01@mail.pucv.cl>
2024-05-26 11:31:38 -0400

    NavBar implemented in SignupPage

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,18 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonToolbar, IonFooter } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonToolbar, IonFooter, IonMenu,IonTitle, IonItem } from '@ionic/react';
 import './css/SignupPage.css'
 import NavBarLogin from '../components/NavBarLogin';
 import Login from '../components/Login';
 import FooterPage from '../components/FooterPage';
 import Signup from '../components/Signup';
-import NavbarSignup from '../components/NavbarSignup';
 
+const paths = [
+  {name: 'Log in', url: '/LoginPage'},
+  {name: 'Sign up', url: '/SignupPage'},
+]
 
 const SignupPage: React.FC = () => {
     return(
     <IonPage>
+    <IonMenu contentId="main-content">
+                <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Menu Content</IonTitle>
+                </IonToolbar>
+                </IonHeader>
+                <IonContent>{paths.map((item,index) =>(
+                  <IonItem routerLink={item.url} routerDirection='none'>{item.name}</IonItem>
+                ))}
+                </IonContent>
+            </IonMenu> 
     <IonHeader>
       <IonToolbar>
-        <NavbarSignup></NavbarSignup>
+        <NavBarLogin></NavBarLogin>
       </IonToolbar>
     </IonHeader>
 
