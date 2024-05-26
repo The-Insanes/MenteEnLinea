ad1657e2bba70f46bbf2192d69495d5e157dc3ac
Joaquin Tapia <joaquin.tapia.m01@mail.pucv.cl>
2024-05-25 22:26:24 -0400

    Attempted to create side menu

--- frontend/src/components/NavBarLogin.tsx
+++ frontend/src/components/NavBarLogin.tsx
@@ -1,19 +1,37 @@
 import './css/NavBarLogin.css';
-import { IonIcon, IonButton, IonImg, IonLabel, IonToolbar, IonMenuButton, IonButtons, IonTitle } from '@ionic/react';
+import { IonIcon, IonButton, IonImg, IonLabel, IonToolbar, IonMenuButton, IonButtons, IonTitle, IonMenuToggle, IonItem, IonContent, IonMenu ,IonRouterOutlet, IonHeader} from '@ionic/react';
 
 interface ContainerProps {
     className?: string | undefined
+    
 };
+const paths = [
+    {name: 'Log in', url: 'LoginPage'},
+    {name: 'Sign up', url: 'SignupPage'},
+]
 
 const NavBarLogin: React.FC<ContainerProps> = ({className}) => {
     return (
+        
         <IonToolbar className={className}>
             <IonButton className='logo_button' fill='clear' slot='start'>
                 <IonImg src='logo.svg'></IonImg>
             </IonButton>
-            <IonButtons slot="end">
-                <IonMenuButton className='option_button' autoHide={false} color='primary'></IonMenuButton>
-            </IonButtons>
+            <IonMenu contentId="main-content">
+                        <IonContent className="ion-padding">
+                            {paths.map((item, index)=> ( 
+                                <IonMenuToggle>
+                                    <IonItem routerLink={item.url} routerDirection='none'>
+                                    {item.name}
+                                    </IonItem>
+                                </IonMenuToggle>
+                            ))}
+                        </IonContent>
+                    </IonMenu>       
+                <IonButtons  id='main-content'slot="end">
+                    <IonMenuButton  className='option_button' autoHide={false} color='primary'></IonMenuButton>
+                </IonButtons>
+            
         </IonToolbar>
     );
 };
