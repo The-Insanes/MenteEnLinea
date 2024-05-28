d0a8aa8b1ff33b9c96089e33dcfabdea6d0b2b36
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-05-28 17:31:45 -0400

    change logo image directory

--- frontend/src/components/NavBarLogin.tsx
+++ frontend/src/components/NavBarLogin.tsx
@@ -2,14 +2,15 @@ import './css/NavBarLogin.css';
 import { IonButton, IonImg, IonToolbar, IonMenuButton, IonButtons } from '@ionic/react';
 
 interface ContainerProps {
-    className?: string | undefined
+    className?: string | undefined,
+    srcLogo?: string | undefined
 };
 
-const NavBarLogin: React.FC<ContainerProps> = ({className}) => {
+const NavBarLogin: React.FC<ContainerProps> = ({className, srcLogo}) => {
     return (
         <IonToolbar className={className}>
             <IonButton className='logo_button' fill='clear' slot='start'>
-                <IonImg src='logo.svg'></IonImg>
+                <IonImg src={srcLogo}></IonImg>
             </IonButton>
             <IonButtons slot="end">
                 <IonMenuButton className='option_button' autoHide={false} color='primary'></IonMenuButton>
