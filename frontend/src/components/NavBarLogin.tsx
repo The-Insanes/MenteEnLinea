a4db1ad9f74070ff911fdcd7235297945abbdcf9
MOWENFML <fabrizzioaml@gmail.com>
2024-05-28 18:34:32 -0400

    small changes

--- frontend/src/components/NavBarLogin.tsx
+++ frontend/src/components/NavBarLogin.tsx
@@ -9,7 +9,7 @@ const NavBarLogin: React.FC<ContainerProps> = ({className}) => {
     return (
         <IonToolbar className={className}>
             <IonButton className='logo_button' fill='clear' slot='start'>
-                <IonImg src='logo.svg'></IonImg>
+                <IonImg src='images/logo.svg'></IonImg>
             </IonButton>
             <IonButtons slot="end">
                 <IonMenuButton className='option_button' autoHide={false} color='primary'></IonMenuButton>
