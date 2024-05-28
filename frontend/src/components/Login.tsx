cca522bf3174744b34a0e0a00da2dd19c3e9cd74
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-05-28 17:31:18 -0400

    add new functionalities to validation

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -7,20 +7,25 @@ import { mail, logoX } from 'ionicons/icons'
 interface ContainerProps { 
     className?: string | undefined,
     src_logo?: string | undefined,
+    verifyEmail?: (email: string) => string | undefined,
+    verifyPassword?: (password: string) => string | undefined,
+    onClick?: () => void
 }
 
-const Login: React.FC<ContainerProps> = ({className, src_logo}) => {
+const Login: React.FC<ContainerProps> = ({className, src_logo, verifyPassword, verifyEmail, onClick}) => {
+
     return (
         <div className={className}>
             <div className='login_base'>
                 <IonImg src={src_logo} alt='logo'></IonImg>
-                <Input className='input_login' label='Correo o Usuario' type='email' placeHolder='email@gmail.com' icon= {mail}></Input>
-                <Input className='input_login' label='Contraseña' type='password' placeHolder='*********'></Input>
+                <Input className='input_login' label='Correo o Usuario' type='email' placeHolder='email@gmail.com' icon= {mail} verifyInput={verifyEmail}></Input>
+                <IonLabel></IonLabel>
+                <Input className='input_login' label='Contraseña' type='password' placeHolder='*********' verifyInput={verifyPassword}></Input>
                 
                 <Divider className='divider'></Divider>
 
                 <IonButton className='buttom_selection' color='light'>
-                    <IonIcon className='buttom_icon' src='google_logo.svg'></IonIcon>
+                    <IonIcon className='buttom_icon' src='images/google_logo.svg'></IonIcon>
                     <IonLabel className='buttom_label'>Google</IonLabel>
                 </IonButton>
 
@@ -30,8 +35,9 @@ const Login: React.FC<ContainerProps> = ({className, src_logo}) => {
                 </IonButton>
 
                 <a className='login_text'>¿Has olvidado tu Contraseña? </a>
-                <IonButton className='login_buttom'>Iniciar Sesión</IonButton>
+                <IonButton className='login_buttom' onClick={onClick}>Iniciar Sesión</IonButton>
             </div>
+            
             <div className='register_base'>
                 <IonLabel>¿No tienes cuenta?</IonLabel>
                 <a>Regístrate</a>
